e40131827ba6759346d5c99a3ed673a29111521b
Weriks <ti15@viacometa.com.br>
2023-09-10 18:14:43 -0400

    refactor: cria area de notificações

---
+++ src/modules/aluno/notificacao/components/MessageList.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container class="pa-0 ma-0 overflow-x-auto">
+    <v-list dense v-if="mensagens.length != 0 ">
+      <v-list-item v-for="(mensagem, i) in mensagens" :key="i">
+        <v-card
+          elevation="0"
+          color="#828282"
+          class="pa-3 mb-3"
+        >
+          <div class="d-flex">
+            <v-card elevation="0" color="#495054" class="pa-3">
+              <v-icon large color="#FFFFFF">mdi-message-text</v-icon>
+            </v-card>
+            <v-card-text class="white--text px-2 py-0 text-truncate">
+              <p>{{ mensagem.text }}</p>
+              <div class="d-flex">
+                <v-icon small color="#FFFFFF" class="mr-2">mdi-clock-time-four</v-icon>
+                <p class="ma-0">{{ mensagem.hour }}</p>
+              </div>
+            </v-card-text>
+          </div>
+        </v-card>
+      </v-list-item>
+    </v-list>
+    <v-card v-else elevation="0" height="70vh" class="d-flex justify-center align-center">Não há mensagens</v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data(){
+    return{ 
+      mensagens:[
+        // {
+        //   text: "Congratulations on completing the firts module", hour: "Just now"
+        // },
+        // {
+        //   text: "Congratulations on completing the firts module", hour: "Just now"
+        // },
+        // {
+        //   text: "Congratulations on completing the firts module", hour: "Just now"
+        // },
+        // {
+        //   text: "Congratulations on completing the firts module", hour: "Just now"
+        // },
+        // {
+        //   text: "Congratulations on completing the firts module", hour: "Just now"
+        // },
+        // {
+        //   text: "Congratulations on completing the firts module", hour: "Just now"
+        // },
+        // {
+        //   text: "Congratulations on completing the firts module", hour: "Just now"
+        // }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/modules/aluno/notificacao/views/Index.vue
+++ src/modules/aluno/notificacao/views/Index.vue
@@ -1,11 +1,45 @@
 <template>
-  <h1>Notificacao</h1>
+  <v-container class="pa-0 ma-0">
+    <v-tabs
+      v-model="tab"
+      fixed-tabs
+      color="#000000"
+    >
+      <v-tab>
+        Mensagens
+      </v-tab>
+      <v-tab>
+        Notificação
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <MessageList />
+      </v-tab-item>
+      <v-tab-item>
+        <NotificationList />
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
 </template>
 
 <script>
+import  MessageList  from "../components/MessageList";
+import  NotificationList  from "../components/NotificationList";
 export default {
 
-  name: "NotificacaoView"
+  name: "NotificacaoView",
+
+  components:{
+    MessageList,
+    NotificationList
+  },
+
+  data () {
+    return {
+      tab: null,
+    }
+  },
 
 }
 </script>
