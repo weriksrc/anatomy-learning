a38b55b56b45b9ca51064e2a73683a438b58ed8b
Weriks <ti15@viacometa.com.br>
2024-04-21 17:11:28 -0300

    feat: cria exemplo de calendario

---
+++ src/modules/professor/agenda/components/Calendario.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center">
+    <v-date-picker v-model="picker"></v-date-picker>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "CalendarioComponent",
+  data() {
+    return {
+      picker: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10)
+    };
+  }
+};
+</script>
+
+<style>
+</style>
