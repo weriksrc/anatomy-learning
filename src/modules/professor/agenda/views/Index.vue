68094d1fdd5c480db2415a49aa1d0f8407f2240c
Weriks <ti15@viacometa.com.br>
2024-04-21 17:11:09 -0300

    refactor: adiciona exemplo de calendario

--- src/modules/professor/agenda/views/Index.vue
+++ src/modules/professor/agenda/views/Index.vue
@@ -1,15 +1,28 @@
 <template>
-  <h1>Resumo</h1>
+  <v-container class="pa-8">
+    <v-row>
+      <v-col cols="12" md="3">
+        <Calendario />
+      </v-col>
+      <v-col cols="12" md="9">
+        <Agenda />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import Agenda from "../components/Agenda.vue";
+import Calendario from "../components/Calendario.vue";
 export default {
+  name: "AgendaView",
 
-  name: "ResumoView"
-
-}
+  components: {
+    Agenda,
+    Calendario
+  }
+};
 </script>
 
 <style>
-
 </style>
