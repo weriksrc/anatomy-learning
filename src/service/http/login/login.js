7cfe2f508414e580854e95a0deabcadcb6de6e27
Weriks <ti15@viacometa.com.br>
2024-04-26 20:38:37 -0400

    feat: adiciona service login

---
+++ src/service/http/login/login.js
@@ -0,0 +1,14 @@
+import BaseServiceHttp from "../baseService";
+
+class UserHttp extends BaseServiceHttp {
+  constructor(id) {
+    const resource = "/login";
+    const relationship = {
+      accessToken: "access-token",
+    };
+    const baseURL = process.env.VUE_APP_ROOT_API;
+    super(id, resource, relationship, baseURL);
+  }
+}
+
+export default (id) => new UserHttp(id);
